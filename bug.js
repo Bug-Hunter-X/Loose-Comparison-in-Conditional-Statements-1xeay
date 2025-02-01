508d34518c24fe1406d2cae314f2c79c731b9357
BugHunter-X <askzenai@gmail.com>
2025-02-01 18:36:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,13 @@
+function checkNumber(num) {
+  if (num > 10) {
+    return "The number is greater than 10";
+  } else if (num < 10) {
+    return "The number is less than 10";
+  } else {
+    return "The number is 10";
+  }
+}
+
+console.log(checkNumber(10)); // Output: The number is 10
+console.log(checkNumber(5));  // Output: The number is less than 10
+console.log(checkNumber(15)); // Output: The number is greater than 10
